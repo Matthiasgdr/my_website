b70b8847bd8e9ddf749dea4127cd2b17679ca9d1
Matthiasgdr <spam68130@gmail.com>
2019-01-29 16:53:03 +0100

    Cleaned Template

--- src/index.js
+++ src/index.js
@@ -3,10 +3,3 @@ import Carousel from './js/Carousel.js'
 import image from './images/image.jpg'
 import fonts from './fonts/heboo-bold.woff2'
 
-console.log('Hello coucou');
-
-const $image = new Image()
-$image.src = image
-document.body.appendChild($image)
-
-const carousel = new Carousel()
